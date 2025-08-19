b7fd2ee4202bc333e06fbe2c7d457b03143e2bd3
jelenarastovic7 <jelenarastovic7@gmail.com>
2025-08-19 19:30:46 +0000

    c

--- src/com/main/app/Config/SecurityConfig.java
+++ src/com/main/app/Config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/register", "/doLogin"))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/register", "/doLogin", "/persons/update"))
                 .authorizeHttpRequests(auth -> auth
  .requestMatchers("/register", "/login", "/doLogin", "/css/**").permitAll()
  .requestMatchers("/admin/panel").hasRole("ADMIN")
